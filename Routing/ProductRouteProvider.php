039b21460afc2f5a82d10b292ea62807d8b212bf
Daniel Rotter <daniel.rotter@massiveart.com>
2014-07-24 17:16:13 +0200

    added dynamic router for products

---
+++ Routing/ProductRouteProvider.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * This file is part of the Sulu CMF.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Sulu\Bundle\ProductBundle\Routing;
+
+use Sulu\Component\Webspace\Analyzer\RequestAnalyzer;
+use Sulu\Component\Webspace\Analyzer\RequestAnalyzerInterface;
+
+class ProductRouteProvider
+{
+    /**
+     * @var RequestAnalyzer
+     */
+    private $requestAnalyzer;
+
+    public function __construct(RequestAnalyzerInterface $requestAnalayzer)
+    {
+        $this->requestAnalyzer = $requestAnalayzer;
+    }
+} 
