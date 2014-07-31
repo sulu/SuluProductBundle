250cf72b56da4fc93f86e9e75dc4e625559cde53
Daniel Rotter <daniel.rotter@massiveart.com>
2014-07-31 17:09:10 +0200

    added product controller as a service

--- Controller/ProductWebsiteController.php
+++ Controller/ProductWebsiteController.php
@@ -10,12 +10,54 @@
 
 namespace Sulu\Bundle\ProductBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sulu\Bundle\ProductBundle\Product\Exception\ProductNotFoundException;
+use Sulu\Bundle\ProductBundle\Product\ProductManagerInterface;
+use Sulu\Component\Webspace\Analyzer\RequestAnalyzerInterface;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProductWebsiteController extends Controller
+/**
+ * This Controller is responsible for the rendering of the product
+ * @package Sulu\Bundle\ProductBundle\Controller
+ */
+class ProductWebsiteController
 {
-    public function displayAction($id) {
-        return new Response($id);
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @var ProductManagerInterface
+     */
+    private $productManager;
+
+    /**
+     * @var RequestAnalyzerInterface
+     */
+    private $requestAnalyzer;
+
+    public function __construct(
+        EngineInterface $templating,
+        ProductManagerInterface $productManager,
+        RequestAnalyzerInterface $requestAnalyzer
+    ) {
+        $this->templating = $templating;
+        $this->productManager = $productManager;
+        $this->requestAnalyzer = $requestAnalyzer;
+    }
+
+    public function displayAction($id)
+    {
+        $product = $this->productManager->findByIdAndLocale(
+            $id,
+            $this->requestAnalyzer->getCurrentLocalization()->getLocalization()
+        );
+
+        if ($product) {
+            return new Response($product->getName());
+        } else {
+            return new Response('404');
+        }
     }
 } 
