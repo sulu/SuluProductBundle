ea9d822432ead1da818dc2464ad16f81a127c93f
Michael Zangerle <michael.zangerle@massiveart.com>
2014-11-17 11:44:52 +0100

    added currencies fixutres and updated list in pricing tab

---
+++ DataFixtures/ORM/Currencies/LoadCurrencies.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of the Sulu CMS.
+ *
+ * (c) MASSIVE ART WebServices GmbH
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Sulu\Bundle\ProductBundle\Entity\Currency;
+
+class LoadCurrencies implements FixtureInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(ObjectManager $manager)
+    {
+        // force id = 1
+        $metadata = $manager->getClassMetaData(get_class(new Currency()));
+        $metadata->setIdGeneratorType(\Doctrine\ORM\Mapping\ClassMetadata::GENERATOR_TYPE_NONE);
+
+        $i = 1;
+        $file = dirname(__FILE__) . '/../../currencies.xml';
+        $doc = new DOMDocument();
+        $doc->load($file);
+
+        $xpath = new DOMXpath($doc);
+        $elements = $xpath->query('/currencies/currency');
+
+        if (!is_null($elements)) {
+            /** @var $element DOMNode */
+            foreach ($elements as $element) {
+                $currency = new Currency();
+                $currency->setId($i);
+                $children = $element->childNodes;
+                /** @var $child DOMNode */
+                foreach ($children as $child) {
+                    switch($child->nodeName){
+                        case "name":
+                            $currency->setName($child->nodeValue);
+                            break;
+                        case "code":
+                            $currency->setCode($child->nodeValue);
+                            break;
+                        case "number":
+                            $currency->setNumber($child->nodeValue);
+                            break;
+                    }
+                }
+                $manager->persist($currency);
+                $i++;
+            }
+        }
+        $manager->flush();
+    }
+}
