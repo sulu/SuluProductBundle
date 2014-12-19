e313d0a08ffaabaeddcd9fb12621178ac1a64a93
Christian Bader <privat@chrisb.at>
2014-12-19 13:15:16 +0100

    Fixed wrong product url

--- Resources/public/js/components/products/main.js
+++ Resources/public/js/components/products/main.js
@@ -129,7 +129,7 @@ define([
 
         triggerWorkflowAction: function(data) {
             if (!!data && !!data.ids && !!data.status) {
-                var url = 'admin/api/products?action=changeState&ids=' + data.ids + '&statusId=' + data.status;
+                var url = '/admin/api/products?action=changeState&ids=' + data.ids + '&statusId=' + data.status;
                 this.sandbox.util.save(url, 'POST')
                     .then(function() {
                         this.sandbox.emit('sulu.product.workflow.completed');
