c5c3702e8b48168c9162187f5991910888fae689
xandrej <andrej.frelih@inbox.com>
2015-03-30 09:11:33 +0200

    Update main.js and twig template

--- Resources/public/js/components/products/components/attributes/main.js
+++ Resources/public/js/components/products/components/attributes/main.js
@@ -13,7 +13,7 @@ define(['config'], function (Config) {
     var formSelector = '#product-attributes-form',
         productAttributesInstanceName = 'product-attribute-list-toolbar',
         datagridInstanceName = 'product-attribute-datagrid',
-        selected = 0,
+        selected = 1,
         /*
         defaults = {
             data: [],
@@ -49,53 +49,47 @@ define(['config'], function (Config) {
                 this.sandbox.emit('sulu.products.list');
             }, this);
 
-            this.sandbox.on('product.state.change', function(id) {
-                alert("OK3");
-                /*
-                if (!this.options.data.status || this.options.data.status.id !== id) {
-                    this.status = {id: id};
-                    this.options.data.status = this.status;
-                    setHeaderBar.call(this, false);
-                }
-                */
-            }, this);
-
-            this.sandbox.on('sulu.products.saved', function(data) {
-                alert("saved56");
-                //setHeaderBar.call(this, true);
-                //this.options.data = data;
-                //this.options.data.status = this.status;
-            }, this);
-
             app.sandbox.on('husky.select.attribute-types.selected.item', function(item) {
-                console.log("drop-down multiple select ddms: selected item: " + item);
-                selected = item;
+                selected = parseInt(item);
             });
 
         },
 
-        callbackFunc = function () {
-            //alert("OK8");
-            save.call(this);
-        },
-
         save = function () {
-            alert("ok9");
-            //this.sandbox.emit('sulu.products.save', data);
+            //this.sandbox.emit('sulu.products.save', this.options.data);
         },
 
         /**
          * called when add icon was clicked
          */
         addButtonClicked = function() {
-            // create overlay
-            //createListOverlay.call(this);
-            var attributeName = this.sandbox.dom.val('#attribute-name');
-            var attributeSelection = this.sandbox.dom.val('.husky-select-label');
-            alert(attributeSelection);
-            app.sandbox.emit('husky.datagrid.' + datagridInstanceName + '.record.add', { value: attributeName }, callbackFunc());
 
-            //alert("OK")
+            var attributeValue = this.sandbox.dom.val('#attribute-name');
+            var attributeName = this.attributeTypes[selected-1].name;
+            //app.sandbox.emit('husky.datagrid.' + datagridInstanceName + '.record.add', { id: selected, value: attributeValue, attributeName: attributeName }, callbackFunc());
+
+            var attributes = this.options.data.attributes;
+
+            var result = _.findWhere(attributes, { 'attributeId': selected });
+
+            if (result) {
+                result.attributeValue = attributeValue;
+                console.log('attribute found');
+                app.sandbox.emit('husky.datagrid.' + datagridInstanceName + '.records.change', { id: selected, value: attributeValue, attributeName: attributeName});
+
+            } else {
+                var new_attribute = {
+                    "attributeId": selected,
+                    "value": attributeValue
+                };
+                console.log('attribute not found');
+                attributes.push(new_attribute);
+                app.sandbox.emit('husky.datagrid.' + datagridInstanceName + '.record.add', { id: selected, value: attributeValue, attributeName: attributeName });
+            }
+            this.options.data.attributes = attributes;
+
+            //this.sandbox.emit('husky.datagrid.records.change', this.options.data);
+            //this.sandbox.emit('husky.datagrid.update');
         },
 
         /**
@@ -110,20 +104,12 @@ define(['config'], function (Config) {
                 resultKey: 'attributes',
                 matchings: [
                     {
-                        name: "id",
-                        content: "id"
-                    },
-                    {
                         name: "value",
                         content: "value"
                     },
                     {
                         name: "attributeName",
                         content: "name"
-                    },
-                    {
-                        name: "attributeTypeName",
-                        content: "type"
                     }
                 ],
                 data: this.options.data
@@ -192,9 +178,25 @@ define(['config'], function (Config) {
                 this.sandbox.start(formSelector);
                 startFormComponents.call(this);
             }.bind(this));
+        };
+
+    return {
+        name: 'Sulu Product Attributes View',
+
+        templates: ['/admin/product/template/product/attributes'],
+
+        /**
+         * is getting called when template is initialized
+         * @param types
+         */
+        setTypes: function(types) {
+            this.attributeTypes = types.attributeTypes;
         },
 
-        render = function () {
+        render: function () {
+
+            this.sandbox.once('sulu.products.set-types', this.setTypes.bind(this));
+
             this.sandbox.dom.html(this.$el, this.renderTemplate('/admin/product/template/product/attributes'));
 
             initForm.call(this);
@@ -205,26 +207,19 @@ define(['config'], function (Config) {
 
             //initForm.call(this, this.options.data);
 
-        };
-
-    return {
-        name: 'Sulu Product Attributes View',
-
-        templates: ['/admin/product/template/product/attributes'],
+        },
 
         initialize: function () {
+            debugger;
             this.status  = !!this.options.data ? this.options.data.status : Config.get('product.status.active');
-
+            this.render();
             //this.options = this.sandbox.util.extend({}, defaults, this.options);
             // merge translations
             //this.translations = this.sandbox.util.extend({}, translations, this.options.translations);
 
             this.options.isEditable = (this.options.isEditable === 'false' || this.options.isEditable === false) ? false : true;
 
-            render.call(this);
-
-            // event listener
-           bindCustomEvents.call(this);
+            bindCustomEvents.call(this);
 
         }
     };
