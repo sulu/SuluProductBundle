bcc0682b1a0ec8a5c066f634e976258342ee4a63
xandrej <andrej.frelih@inbox.com>
2015-05-20 14:37:12 +0200

    repair if statement

--- Api/Product.php
+++ Api/Product.php
@@ -735,8 +735,8 @@ class Product extends ApiWrapper implements ApiProductInterface
                     $startDate = $specialPriceEntity->getStartDate();
                     $endDate = $specialPriceEntity->getEndDate();
                     $now = new \DateTime();
-                    if ($now >= $startDate && $now <= $endDate || $now >= $startDate && empty($endDate)
-                    || empty($startDate) && empty($endDate)) {
+                    if (($now >= $startDate && $now <= $endDate) || ($now >= $startDate && empty($endDate))
+                    || (empty($startDate) && empty($endDate))) {
                         $specialPrice = $specialPriceEntity->getPrice();
                     } else {
                         $specialPrice = null;
