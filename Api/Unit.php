79a13236d27ed33cd92a48b80e538e8a4311166c
turbo-ele <eliash@gmx.at>
2015-12-04 11:14:22 +0100

    bugfix when unit has no translation

--- Api/Unit.php
+++ Api/Unit.php
@@ -10,17 +10,16 @@
 
 namespace Sulu\Bundle\ProductBundle\Api;
 
-use Sulu\Bundle\ProductBundle\Entity\Unit as Entity;
 use JMS\Serializer\Annotation\VirtualProperty;
 use JMS\Serializer\Annotation\SerializedName;
 use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Groups;
+use Sulu\Bundle\ProductBundle\Entity\Unit as Entity;
 use Sulu\Component\Rest\ApiWrapper;
 
 /**
  * The unit class which will be exported to the api
  *
- * @package Sulu\Bundle\ProductBundle\Api
  * @ExclusionPolicy("all")
  */
 class Unit extends ApiWrapper
@@ -36,10 +35,12 @@ class Unit extends ApiWrapper
     }
 
     /**
-     * The id of the type
-     * @return int The id of the type
+     * The id of the type.
+     *
      * @VirtualProperty
      * @SerializedName("id")
+     *
+     * @return int The id of the type
      */
     public function getId()
     {
@@ -47,14 +48,20 @@ class Unit extends ApiWrapper
     }
 
     /**
-     * The name of the type
-     * @return int The name of the type
+     * The name of the type.
+     *
      * @VirtualProperty
      * @SerializedName("name")
      * @Groups({"cart"})
+     *
+     * @return int The name of the type
      */
     public function getName()
     {
+        if (!$this->entity->getTranslation($this->locale)) {
+            return null;
+        }
+        
         return $this->entity->getTranslation($this->locale)->getName();
     }
-} 
+}
