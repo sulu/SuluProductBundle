962f477328fadbeb3cada44ebd264b36120996e0
turbo-ele <eliash@gmx.at>
2016-01-27 15:30:35 +0100

    Fixed setting of enddate when null

--- Product/ProductManager.php
+++ Product/ProductManager.php
@@ -1136,8 +1136,11 @@ class ProductManager implements ProductManagerInterface
 
                 if (isset($specialPriceData['endDate'])) {
                     $endDate = $this->checkDateString($specialPriceData['endDate']);
-                    // set time to 23:59:59
-                    $endDate->setTime(23, 59, 59);
+
+                    if ($endDate) {
+                        // set time to 23:59:59
+                        $endDate->setTime(23, 59, 59);
+                    }
                     $specialPrice->setEndDate($endDate);
                 }
             }
